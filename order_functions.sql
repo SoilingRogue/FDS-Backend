c644dd7fce49d3cb3d465773d3f0741cff571da6
Yeo Tong <helloimyeotong@gmail.com>
2020-05-06 03:34:55 +0800

    add addOrder function(not complete)

--- order_functions.sql
+++ order_functions.sql
@@ -14,3 +14,33 @@ BEGIN
 
 END;
 $$ LANGUAGE 'plpgsql';
+
+-- * Not Completed cause need schedule stuff to be done first
+-- Record new order and assign rider to newly created order
+DROP TYPE IF EXISTS FoodItemQty;
+CREATE TYPE FoodItemQty AS (rid INTEGER, foodName VARCHAR(50), qty INTEGER);
+DROP FUNCTION IF EXISTS addOrder;
+CREATE FUNCTION addOrder(foodItemsArr FoodItemQty[], newFoodCost FLOAT, 
+	newDeliveryCost FLOAT, newTotalCost FLOAT, newPointsUsed INTEGER, newDeliverLocation TEXT)
+ RETURNS VOID
+AS $$
+DECLARE
+ newFoodItem FoodItemQty[];
+BEGIN
+ 	
+ -- Update stock
+ FOREACH newFoodItem IN ARRAY foodItemsArr
+ LOOP
+ 	UPDATE FoodItems SET currentStock = (currentStock - newFoodItem.qty) 
+ 	WHERE (newFoodItem.rid, newFoodItem.foodName) = (rid, foodName);
+ END LOOP;
+
+ -- Insert into Order
+ INSERT INTO Orders(foodCost, deliveryCost, totalCost, pointsUsed, ordered_at, deliverLocation) 
+ VALUES(newFoodCost, newDeliveryCost, newTotalCost, newPointsUsed, DEFAULT, newDeliverLocation);
+
+ -- Assign rider (need find currently working rider then find which one has least pending orders)
+
+
+END;
+$$ LANGUAGE 'plpgsql';
