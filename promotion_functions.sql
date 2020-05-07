6da982b3a593d1c9bd2986831e83d4a0ed45be58
SoilingRogue <yuan.xinran.stanley@gmail.com>
2020-05-07 21:55:14 +0800

    Updates

--- promotion_functions.sql
+++ promotion_functions.sql
@@ -14,7 +14,6 @@ DECLARE
     time varchar(8);
 BEGIN
     SELECT convert(varchar(8), NOW(), 120) INTO time;
-    SELECT rid FROM 
     RETURN QUERY 
     SELECT DISTINCT pid
     FROM FirstOrderPromotions, (SELECT * FROM Places WHERE uid = inputUid) AS F
@@ -38,9 +37,30 @@ $$ LANGUAGE 'plpgsql';
 DROP FUNCTION IF EXISTS applyPromotion;
 CREATE FUNCTION applyPromotion(promoUid INTEGER, newFoodCost FLOAT, newDeliveryCost FLOAT, 
     newTotalCost FLOAT)
-RETURNS setof INTEGER
+RETURNS INTEGER
 AS $$
+DECLARE
+    finalFoodCost FLOAT;
+    finalDeliveryCost FLOAT;
+    finalTotalCost FLOAT;
 BEGIN
-    RETURN QUERY SELECT ;
+    IF (SELECT count(*) FROM FirstOrderPromotions WHERE pid = promoUid) > 0 THEN
+        SELECT newTotalCost * (1 - F.discountPercentage) INTO finalTotalCost FROM FirstOrderPromotions F WHERE pid = promoUid;
+        SELECT 0.0 into finalFoodCost;
+        SELECT 0.0 into finalDeliveryCost;
+    ELSE
+        IF (SELECT count(*) FROM DeliveryPromotions WHERE pid = promoUid) > 0 THEN
+            SELECT newDeliveryCost * D.discountPercentage INTO finalDeliveryCost FROM DeliveryPromotions D WHERE pid = promoUid;
+            SELECT 0.0 into finalFoodCost;
+            SELECT newTotalCost - finalDeliveryCost into finalTotalCost;
+            SELECT 0.0 into finalDeliveryCost;
+        ELSE
+            SELECT newFoodCost * discountPercentage INTO finalFoodCost FROM PriceTimeOrderPromotions WHERE pid = promoUid;
+            SELECT newTotalCost - finalFoodCost into finalTotalCost;
+            SELECT 0.0 into finalFoodCost;
+            SELECT 0.0 into finalDeliveryCost;
+        END IF;
+    END IF;
+    RETURN finalFoodCost + finalDeliveryCost + finalTotalCost;
 END;
 $$ LANGUAGE 'plpgsql';
