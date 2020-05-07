4e2d4ee26b515ff3f1989a2e54a2e164dd1395db
SoilingRogue <yuan.xinran.stanley@gmail.com>
2020-05-07 21:29:58 +0800

    Edits to promo functions

--- promotion_functions.sql
+++ promotion_functions.sql
@@ -2,46 +2,45 @@
 /*
 * Functions:
 * getFoodData(): Get promotions the order can receive
-* getRestaurants(): Use actual promotion
+* getRestaurants(): Apply actual promotion
 */
 
 DROP FUNCTION IF EXISTS getValidPromotions;
-CREATE FUNCTION getValidPromotions(inputUid INTEGER)
+CREATE FUNCTION getValidPromotions(inputUid INTEGER, foodItemsArr FoodItemQty[], newFoodCost FLOAT, 
+	newDeliveryCost FLOAT, newTotalCost FLOAT, restId INTEGER)
+RETURNS setof INTEGER
+AS $$
+DECLARE
+    time varchar(8);
+BEGIN
+    SELECT convert(varchar(8), NOW(), 120) INTO time;
+    SELECT rid FROM 
+    RETURN QUERY 
+    SELECT DISTINCT pid
+    FROM FirstOrderPromotions, (SELECT * FROM Places WHERE uid = inputUid) AS F
+    WHERE inputUid <> uid
+    UNION
+    SELECT DISTINCT pid
+    FROM DeliveryPromotions D
+    WHERE newDeliveryCost >= baseAmount
+    AND time >= convert(varchar(8), D.startDate, 120)
+    AND time <= convert(varchar(8), D.endDate, 120)
+    UNION
+    SELECT DISTINCT pid
+    FROM FirstOrderPromotions natural join HasPromotions natural join RestaurantPromotions
+    WHERE restId = rid AND basePrice <= newFoodCost
+    AND time >= convert(varchar(8), startDate, 120)
+    AND time <= convert(varchar(8), endDate, 120);
+END;
+$$ LANGUAGE 'plpgsql';
+
+
+DROP FUNCTION IF EXISTS applyPromotion;
+CREATE FUNCTION applyPromotion(promoUid INTEGER, foodItemsArr FoodItemQty[], newFoodCost FLOAT, 
+	newDeliveryCost FLOAT, newTotalCost FLOAT)
 RETURNS setof INTEGER
 AS $$
 BEGIN
     RETURN QUERY SELECT ;
-END;
-$$ LANGUAGE 'plpgsql';
-
-
-
-
--- Get Food Data
-DROP FUNCTION IF EXISTS getFoodData;
-CREATE FUNCTION getFoodData()
-RETURNS setof json
-AS $$
-BEGIN
- 
- RETURN QUERY SELECT array_to_json(array_agg(row_to_json(t))) FROM (
-    SELECT F1.foodName, R1.rName, R1.rid, F1.price, F1.availability, F1.dailyLimit - F1.currentOrders
-    FROM FoodItems F1 JOIN Restaurants R1 USING (rid)
-    ) t;
-
-END;
-$$ LANGUAGE 'plpgsql';
-
--- Get Food Data
-DROP FUNCTION IF EXISTS getRestaurants;
-CREATE FUNCTION getRestaurants()
- RETURNS setof json
-AS $$
-BEGIN
- 
- RETURN QUERY SELECT array_to_json(array_agg(row_to_json(t))) FROM (
-    SELECT R.rid, R.rName
-    FROM Restaurants R) t;
-
 END;
 $$ LANGUAGE 'plpgsql';
