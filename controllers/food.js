03156c0cce35523bf8e2710092bc72f42b2a092e
raikonen <choozeyuan97@gmail.com>
2020-05-13 17:18:22 +0800

    fix

--- controllers/food.js
+++ controllers/food.js
@@ -11,6 +11,20 @@ const getFoodData = (req, res, db) => {
         })
 }
 
+const getRestaurants = (req, res, db) => {
+    db.query(
+        `select getRestaurants()`,
+        (error, results) => {
+            if (error) {
+                console.log(error)
+                res.status(400).json({ error: `DB error: ${error}` })
+                return
+            }
+            res.status(200).json(results.rows[0]['getrestaurants'])
+        })
+}
+
 module.exports = {
     getFoodData,
+    getRestaurants,
 }
